32e8b2ad2ae410c6aed050388f18d78bbfb12838
jason81508 <otisqqqq@gmail.com>
2022-05-28 14:41:48 +0800

    5/28 細節處理

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+// nav toggle
 $(document).ready(function() {
     $('.nav-toggler').on('click',  function(e){
        e.preventDefault();
        $('body').toggleClass('menu-show');
    });
  });
+
+//  text typing
+var typed = new Typed(".new-type",{
+  strings:["","Web Designer","Web Developer","Graphic Designer"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
